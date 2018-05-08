fb4cd82a1d34497073fbbcde652956c6ea23a8a8
xzx <xiezhenxiang@hiekn.com>
2018-05-08 11:29:23 +0800

    init

--- src/test/java/com/manager/common/ManagerApplicationTests.java
+++ src/test/java/com/manager/common/ManagerApplicationTests.java
@@ -1,16 +1,21 @@
 package com.manager.common;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
+
 public class ManagerApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Ignore
+    @Test
+    public void contextLoads() {
+    }
 
 }
