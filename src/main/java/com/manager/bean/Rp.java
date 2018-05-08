e3978fe5b86d336d35b1b3d3d21eeb8e9c1e40b7
xzx <xiezhenxiang@hiekn.com>
2018-05-08 10:41:43 +0800

    init

---
+++ src/main/java/com/manager/bean/Rp.java
@@ -0,0 +1,23 @@
+package com.manager.bean;
+
+public class Rp {
+    private Integer rid;
+
+    private Integer pid;
+
+    public Integer getRid() {
+        return rid;
+    }
+
+    public void setRid(Integer rid) {
+        this.rid = rid;
+    }
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+}
